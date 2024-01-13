14bd17021aa650734ffef9bbe1d6d430ab3ecd43
Ugur <ugurarslanpeace@gmail.com>
2024-01-13 16:42:36 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h2>Expense Tracker by Ugur</h2>;
+}
+
+export default Header;
---
+++ src/components/Transaction.jsx
@@ -0,0 +1,24 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
+function Transaction({ transaction }) {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+}
+
+export default Transaction;
